0f757bc3534a1470760a22765db362c7c5d005f6
danielevanc <danielevanc2@gmail.com>
2019-05-31 01:54:43 +0700

    test case

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function haCounter(arr) {
+var stringke = arr.split ('')
 
-}
 
-function theLaughter(arr) {
+var mapnya = stringke.map(function(x){
+  
+  if (x === 'h'){
+  stringke.shift()
+  }
+})
+return stringke.length
+
 
 }
 
@@ -10,10 +17,10 @@ function theLaughter(arr) {
 console.log(haCounter('haha')) // 2
 console.log(haCounter('hahaha')) // 3
 
-console.log(theLaughter([
-  ['Tony', 'haha, get lost, squidward!'],
-  ['Mantis', 'hahahahahahaha'],
-  ['Thanos', 'I am inevitable'],
-  ['Rocket', 'Boom! hahahaha']
-])) // 'Mantis'
+// console.log(theLaughter([
+//   ['Tony', 'haha, get lost, squidward!'],
+//   ['Mantis', 'hahahahahahaha'],
+//   ['Thanos', 'I am inevitable'],
+//   ['Rocket', 'Boom! hahahaha']
+// ])) // 'Mantis'
 
