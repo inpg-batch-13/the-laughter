1f1f4d283c067efc71b996d66367e8f3f808d1c6
rahmatrhd <kickyou44@gmail.com>
2019-06-17 21:09:18 +0700

    solve the laughter

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-function haCounter(arr) {
-
+function haCounter(text) {
+  return text
+    .replace(/ha/g, '*')
+    .split('')
+    .filter(x => x === '*')
+    .length
 }
 
 function theLaughter(arr) {
+  var count = haCounter(arr[0][1])
+  var result = arr[0][0]
 
+  for (var i = 0; i < arr.length; i++) {
+    if (haCounter(arr[i][1]) > count) {
+      count = haCounter(arr[i][1])
+      result = arr[i][0]
+    }
+  }
+
+  return result
 }
 
 // TEST CASE
-console.log(haCounter('haha')) // 2
-console.log(haCounter('hahaha')) // 3
+// console.log(haCounter('haha')) // 2
+// console.log(haCounter('hahaha')) // 3
+// console.log(haCounter('hahahiihi')) // 2
 
 console.log(theLaughter([
   ['Tony', 'haha, get lost, squidward!'],
   ['Mantis', 'hahahahahahaha'],
   ['Thanos', 'I am inevitable'],
-  ['Rocket', 'Boom! hahahaha']
+  ['Rocket', 'Boom! hahahahahahahaha']
 ])) // 'Mantis'
 
